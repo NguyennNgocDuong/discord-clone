9291138cba9545af439546399e1cb17588c69e84
Nguyễn Ngọc Dương <nguyenngocduong220901@gmail.com>
2023-10-12 09:55:01 +0700

    Discord clone

---
+++ src/app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/init-profile";
+import React from "react";
+import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/init-modal";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />;
+};
+
+export default SetupPage;
